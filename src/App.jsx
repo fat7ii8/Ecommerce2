03188d3fb406e79653441a397b68f6309cbd598f
fat7ii8 <abdofathy068@gmail.com>
2025-07-20 09:05:57 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
 import Home from "./Pages/Home/Home";
+import Navbar from "./Components/Navbar/Navbar";
 import Layout from "./Components/Layout/Layout";
 import { Toaster } from "react-hot-toast";
 import WriteEmail from "./Pages/WriteEmail/WriteEmail";
@@ -14,7 +15,7 @@ import CartProvider from "./Components/Context/Cart.context";
 import Cart from "./Pages/Cart/Cart";
 import ProductDetails from "./Pages/ProductDetails/ProductDetails";
 import Checkout from "./Pages/Checkout/Checkout";
-import Orders from "./Pages/Ordars/ALLOrdars";
+import Ordars from "./Pages/Ordars/ALLOrdars";
 import Brands from "./Pages/Brands/Brands";
 import BrandProvider from "./Components/Context/brand.context";
 import Productss from "./Pages/Products/Productss";
@@ -22,62 +23,107 @@ import Category from "./Pages/Category/Category";
 import WishList from "./Pages/WishList/WishList";
 import WishListProvider from "./Components/Context/WishList.context";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import NotFound from "./Pages/NotFound/NotFound"; // Optional fallback page
 
-const routes = createHashRouter([
+const routes = createBrowserRouter([
   {
-    path: "/",
+    path: "",
     element: (
       <ProtectedRoutes>
-        <Layout />
+        <Layout></Layout>
       </ProtectedRoutes>
     ),
     children: [
-      { index: true, element: <Home /> },
-      { path: "home", element: <Home /> },
-      { path: "cart", element: <Cart /> },
-      { path: "product/:id", element: <ProductDetails /> },
-      { path: "checkout", element: <Checkout /> },
-      { path: "allorders", element: <Orders /> },
-      { path: "brands", element: <Brands /> },
-      { path: "products", element: <Productss /> },
-      { path: "categories", element: <Category /> },
-      { path: "wishlist", element: <WishList /> },
+      {
+        index: true,
+        element: <Home></Home>,
+      },
+      {
+        path: "home",
+        element: <Home></Home>,
+      },
+      {
+        path: "cart",
+        element: <Cart></Cart>,
+      },
+      {
+        path: "Product/:id",
+        element: <ProductDetails></ProductDetails>,
+      },
+      {
+        path: "checkout",
+        element: <Checkout></Checkout>,
+      },
+      {
+        path: "allorders",
+        element: <Ordars></Ordars>,
+      },
+      {
+        path: "Brands",
+        element: <Brands></Brands>,
+      },
+      {
+        path: "products",
+        element: <Productss></Productss>,
+      },
+      {
+        path: "categories",
+        element: <Category></Category>,
+      },
+      {
+        path: "wishlist",
+        element: <WishList></WishList>,
+      },
     ],
   },
   {
-    path: "/",
+    path: "",
     element: (
       <GardRouteForLogin>
-        <Layout />
+        <Layout></Layout>
       </GardRouteForLogin>
     ),
     children: [
-      { path: "login", element: <Login /> },
-      { path: "register", element: <Register /> },
-      { path: "writeemail", element: <WriteEmail /> },
-      { path: "recivecode", element: <ReciveCode /> },
-      { path: "resetpassword", element: <ResetPassword /> },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+      {
+        path: "register",
+        element: <Register></Register>,
+      },
+      {
+        path: "WriteEmail",
+        element: <WriteEmail></WriteEmail>,
+      },
+      {
+        path: "Recivecode",
+        element: <ReciveCode></ReciveCode>,
+      },
+      {
+        path: "ResetPassword",
+        element: <ResetPassword></ResetPassword>,
+      },
     ],
   },
-]);
+], { basename: '/Ecommerce2' });
 
-const queryClient = new QueryClient();
-
+const x = new QueryClient();
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TokenProvider>
-        <WishListProvider>
-          <BrandProvider>
-            <CartProvider>
-              <RouterProvider router={routes} />
-              <Toaster />
-            </CartProvider>
-          </BrandProvider>
-        </WishListProvider>
-      </TokenProvider>
-    </QueryClientProvider>
+    <>
+      <QueryClientProvider client={x}>
+        <TokenProvider>
+          <WishListProvider>
+            <BrandProvider>
+              <CartProvider>
+                <RouterProvider router={routes}></RouterProvider>
+                <Toaster></Toaster>
+              </CartProvider>
+            </BrandProvider>
+          </WishListProvider>
+        </TokenProvider>
+      </QueryClientProvider>
+    </>
   );
 }
 
