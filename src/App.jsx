8fab3e8f97a64d7db36350af8814609404891bda
fat7ii8 <abdofathy068@gmail.com>
2025-07-20 08:53:32 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
 import Home from "./Pages/Home/Home";
-import Navbar from "./Components/Navbar/Navbar";
 import Layout from "./Components/Layout/Layout";
 import { Toaster } from "react-hot-toast";
 import WriteEmail from "./Pages/WriteEmail/WriteEmail";
@@ -15,7 +14,7 @@ import CartProvider from "./Components/Context/Cart.context";
 import Cart from "./Pages/Cart/Cart";
 import ProductDetails from "./Pages/ProductDetails/ProductDetails";
 import Checkout from "./Pages/Checkout/Checkout";
-import Ordars from "./Pages/Ordars/ALLOrdars";
+import Orders from "./Pages/Ordars/ALLOrdars";
 import Brands from "./Pages/Brands/Brands";
 import BrandProvider from "./Components/Context/brand.context";
 import Productss from "./Pages/Products/Productss";
@@ -23,109 +22,68 @@ import Category from "./Pages/Category/Category";
 import WishList from "./Pages/WishList/WishList";
 import WishListProvider from "./Components/Context/WishList.context";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import NotFound from "./Pages/NotFound/NotFound"; // You’ll need to create this component
 
-const routes = createBrowserRouter([
+const routes = createBrowserRouter(
+  [
+    {
+      path: "",
+      element: (
+        <ProtectedRoutes>
+          <Layout />
+        </ProtectedRoutes>
+      ),
+      children: [
+        { index: true, element: <Home /> },
+        { path: "home", element: <Home /> },
+        { path: "cart", element: <Cart /> },
+        { path: "product/:id", element: <ProductDetails /> },
+        { path: "checkout", element: <Checkout /> },
+        { path: "allorders", element: <Orders /> },
+        { path: "brands", element: <Brands /> },
+        { path: "products", element: <Productss /> },
+        { path: "categories", element: <Category /> },
+        { path: "wishlist", element: <WishList /> },
+      ],
+    },
+    {
+      path: "",
+      element: (
+        <GardRouteForLogin>
+          <Layout />
+        </GardRouteForLogin>
+      ),
+      children: [
+        { path: "login", element: <Login /> },
+        { path: "register", element: <Register /> },
+        { path: "writeemail", element: <WriteEmail /> },
+        { path: "recivecode", element: <ReciveCode /> },
+        { path: "resetpassword", element: <ResetPassword /> },
+      ],
+    },
+  ],
   {
-    path: "",
-    element: (
-      <ProtectedRoutes>
-        <Layout></Layout>
-      </ProtectedRoutes>
-    ),
-    children: [
-      {
-        index: true,
-        element: <Home></Home>,
-      },
-      {
-        path: "home",
-        element: <Home></Home>,
-      },
-      {
-        path: "cart",
-        element: <Cart></Cart>,
-      },
-      {
-        path: "Product/:id",
-        element: <ProductDetails></ProductDetails>,
-      },
-      {
-        path: "checkout",
-        element: <Checkout></Checkout>,
-      },
-      {
-        path: "allorders",
-        element: <Ordars></Ordars>,
-      },
-      {
-        path: "Brands",
-        element: <Brands></Brands>,
-      },
-      {
-        path: "products",
-        element: <Productss></Productss>,
-      },
-      {
-        path: "categories",
-        element: <Category></Category>,
-      },
-      {
-        path: "wishlist",
-        element: <WishList></WishList>,
-      },
-    ],
-  },
-  {
-    path: "",
-    element: (
-      <GardRouteForLogin>
-        <Layout></Layout>
-      </GardRouteForLogin>
-    ),
-    children: [
-      {
-        path: "login",
-        element: <Login></Login>,
-      },
-      {
-        path: "register",
-        element: <Register></Register>,
-      },
-      {
-        path: "WriteEmail",
-        element: <WriteEmail></WriteEmail>,
-      },
-      {
-        path: "Recivecode",
-        element: <ReciveCode></ReciveCode>,
-      },
-      {
-        path: "ResetPassword",
-        element: <ResetPassword></ResetPassword>,
-      },
-    ],
-  },
-], {basename: "/Ecommerce2"});
+    basename: "/Ecommerce2",
+  }
+);
 
-const x = new QueryClient();
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <>
-      <QueryClientProvider client={x}>
-        <TokenProvider>
-          <WishListProvider>
-            <BrandProvider>
-              <CartProvider>
-                <RouterProvider router={routes}></RouterProvider>
-                <Toaster></Toaster>
-              </CartProvider>
-            </BrandProvider>
-          </WishListProvider>
-        </TokenProvider>
-        {/* <ReactQueryDevtools initialIsOpen={false}></ReactQueryDevtools> */}
-      </QueryClientProvider>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <TokenProvider>
+        <WishListProvider>
+          <BrandProvider>
+            <CartProvider>
+              <RouterProvider router={routes} />
+              <Toaster />
+            </CartProvider>
+          </BrandProvider>
+        </WishListProvider>
+      </TokenProvider>
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    </QueryClientProvider>
   );
 }
 
